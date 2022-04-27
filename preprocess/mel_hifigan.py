88db2e1901b3beae0c498a465958859e88ad58b3
hongchengzhu <hongchengz@whu.edu.cn>
2022-04-27 15:07:52 +0800

    multi thread processing

--- preprocess/mel_hifigan.py
+++ preprocess/mel_hifigan.py
@@ -3,32 +3,60 @@ import numpy as np
 import soundfile as sf
 from tqdm import tqdm
 import torch
+import threading
 import sys
 sys.path.append('/home/hongcz/alab/code')
 print(sys.path)
 from hifi_gan_master.meldataset import mel_spectrogram
 
 # audio file directory
-rootDir = '/home/hongcz/alab/data/LJSpeech-1.1/wavs16'
+rootDir = '/home/hongcz/alab/data/VCTK-Corpus/wav16'
 # spectrogram directory
-targetDir = '/home/hongcz/alab/feature/mel_hifigan_padding'
+targetDir = '/home/hongcz/alab/feature/mel_hifigan_padding_alignment_VCTK/'
+if not os.path.exists(targetDir):
+    os.mkdir(targetDir)
+
+global spklist
+spklist = sorted(os.listdir(rootDir))
 
 
-filelist = os.listdir(rootDir)
-for wavname in tqdm(sorted(filelist)):
+def mel_hifigan_extractor(index):
+    spk = spklist[index]
+    src = os.path.join(rootDir, spk)
+    wav_list = os.listdir(src)
     # Read audio file
-    x, fs = sf.read(os.path.join(rootDir, wavname))
-    x = torch.FloatTensor(x).unsqueeze(0)
-    # extract mel-spectrogram
-    mel = mel_spectrogram(x, 400, 80, 16000, 320, 400, 0, 8000, center=False)
-    # save spect
-    # if mel.shape[0] > 128:
-    mel = mel.transpose(1, 2).squeeze(0).cpu().numpy().astype(np.float32)
-    tag = mel.shape[0] % 4
-    if tag != 0:
-        mel_padding = np.zeros([mel.shape[0] + (4 - tag), 80])
-        mel_padding[:mel.shape[0], :] = mel
-    else:
-        mel_padding = mel
-    np.save(os.path.join(targetDir, wavname[:-4]), mel_padding.astype('float32'), allow_pickle=False)
+    for wavname in wav_list:
+        x, fs = sf.read(os.path.join(src, wavname))
+        x = torch.FloatTensor(x).unsqueeze(0)
+        # extract mel-spectrogram
+        mel = mel_spectrogram(x, 400, 80, 16000, 320, 400, 0, 8000, center=False)
+        # save spect
+        # if mel.shape[0] > 128:
+        mel = mel.transpose(1, 2).squeeze(0).cpu().numpy().astype(np.float32)
+        tag = mel.shape[0] % 4
+        if tag != 0:
+            mel_padding = np.zeros([mel.shape[0] + (4 - tag), 80])
+            mel_padding[:mel.shape[0], :] = mel
+        else:
+            mel_padding = mel
+        tgt = os.path.join(targetDir, spk)
+        if not os.path.exists(tgt):
+            os.mkdir(tgt)
+        np.save(os.path.join(tgt, wavname[:-4]), mel_padding.astype('float32'), allow_pickle=False)
 
+
+for i in range(0, 109, 4):
+    t1 = threading.Thread(target=mel_hifigan_extractor, args=(i, ))
+    t2 = threading.Thread(target=mel_hifigan_extractor, args=(i+1, ))
+    t3 = threading.Thread(target=mel_hifigan_extractor, args=(i+2, ))
+    t4 = threading.Thread(target=mel_hifigan_extractor, args=(i+3, ))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
--- preprocess/wac2vec_extractor.py
+++ preprocess/wac2vec_extractor.py
@@ -5,6 +5,7 @@ import pickle
 import s3prl.hub as hub
 import soundfile as sf
 from tqdm import tqdm
+import threading
 
 
 device = 'cuda:0'
@@ -12,26 +13,53 @@ device = 'cuda:0'
 wav2vec_model = getattr(hub, 'wav2vec2')()
 wav2vec_model = wav2vec_model.to(device)
 
-# wavs = [torch.randn(180000, dtype=torch.float).to(device)]
+
+root = '/home/hongcz/alab/data/VCTK-Corpus/wav16'
+target = '/home/hongcz/alab/feature/wav2vec2_padding_VCTK/'
+if not os.path.exists(target):
+    os.mkdir(target)
+
+global spklist
+spklist = sorted(os.listdir(root))
 
 
-with torch.no_grad():
-    wav_dir = '/home/hongcz/alab/data/LJSpeech-1.1/wavs16'
-    wavs = []
-    wav_list = os.listdir(wav_dir)
-    for wavname in tqdm(sorted(wav_list)):
-        wav, _ = sf.read(os.path.join(wav_dir, wavname))
-        wav = torch.tensor(wav, dtype=torch.float32).to(device).unsqueeze(0)
-        # wavs.append(wav)
-        ret = wav2vec_model(wav)['last_hidden_state']
-        ret = ret.cpu()
-        tag = ret.shape[1] % 4
-        if tag != 0:
-            ret_padding = torch.zeros([1, ret.shape[1] + (4 - tag), 768])
-            ret_padding[:, :ret.shape[1], :] = ret
-        else:
-            ret_padding = ret
-        to_name = '/home/hongcz/alab/feature/wav2vec2_padding/' + wavname[:-4] + '.pkl'
-        with open(to_name, 'wb') as f:
-            pickle.dump(ret_padding, f)
-    print(1)
+def extract_wav2vec2(index):
+    with torch.no_grad():
+        spk = spklist[index]
+        src = os.path.join(root, spk)
+        wav_list = os.listdir(src)
+        for wavname in tqdm(wav_list):
+            wav, _ = sf.read(os.path.join(src, wavname))
+            wav = torch.tensor(wav, dtype=torch.float32).to(device).unsqueeze(0)
+            ret = wav2vec_model(wav)['last_hidden_state']
+            ret = ret.cpu()
+            tag = ret.shape[1] % 4
+            if tag != 0:
+                ret_padding = torch.zeros([1, ret.shape[1] + (4 - tag), 768])
+                ret_padding[:, :ret.shape[1], :] = ret
+            else:
+                ret_padding = ret
+            tgt = os.path.join(target, spk)
+            if not os.path.exists(tgt):
+                os.mkdir(tgt)
+            to_name = os.path.join(tgt, wavname[:-4] + '.pkl')
+            with open(to_name, 'wb') as f:
+                pickle.dump(ret_padding, f)
+        print(1)
+
+
+for i in range(0, 109, 4):
+    t1 = threading.Thread(target=extract_wav2vec2, args=(i, ))
+    t2 = threading.Thread(target=extract_wav2vec2, args=(i+1, ))
+    t3 = threading.Thread(target=extract_wav2vec2, args=(i+2, ))
+    t4 = threading.Thread(target=extract_wav2vec2, args=(i+3, ))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
