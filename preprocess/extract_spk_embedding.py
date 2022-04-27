6c9fdf10fd37213aa11693ffc9263ef5e34dda47
hongchengzhu <hongchengz@whu.edu.cn>
2022-04-27 15:46:46 +0800

    multi thread d-vector spk embedding exatractor

--- preprocess/extract_spk_embedding.py
+++ preprocess/extract_spk_embedding.py
@@ -7,9 +7,10 @@ from model_bl import D_VECTOR
 from collections import OrderedDict
 import numpy as np
 import torch
+import threading
 
 C = D_VECTOR(dim_input=80, dim_cell=768, dim_emb=256).eval().cuda()
-c_checkpoint = torch.load('./checkpoint/speaker_encoder/3000000-BL.ckpt')
+c_checkpoint = torch.load('../checkpoint/speaker_encoder/3000000-BL.ckpt')
 new_state_dict = OrderedDict()
 for key, val in c_checkpoint['model_b'].items():
     new_key = key[7:]
@@ -19,17 +20,20 @@ num_uttrs = 10
 len_crop = 128
 
 # Directory containing mel-spectrograms
-rootDir = './spmel'
+rootDir = '/home/hongcz/alab/feature/mel_hifigan_padding_alignment_VCTK'
+targetDir = '/home/hongcz/alab/feature/speaker_embedding_VCTK'
+if not os.path.exists(targetDir):
+    os.mkdir(targetDir)
 dirName, subdirList, _ = next(os.walk(rootDir))
+sorted(dirName)
+sorted(subdirList)
 print('Found directory: %s' % dirName)
 
 
-speakers = []
-for speaker in sorted(subdirList):
+def spk_embedding(index):
+    speaker = subdirList[index]
     print('Processing speaker: %s' % speaker)
-    utterances = []
-    utterances.append(speaker)
-    _, _, fileList = next(os.walk(os.path.join(dirName,speaker)))
+    _, _, fileList = next(os.walk(os.path.join(dirName, speaker)))
     
     # make speaker embedding
     assert len(fileList) >= num_uttrs
@@ -39,7 +43,7 @@ for speaker in sorted(subdirList):
         tmp = np.load(os.path.join(dirName, speaker, fileList[idx_uttrs[i]]))
         candidates = np.delete(np.arange(len(fileList)), idx_uttrs)
         # choose another utterance if the current one is too short
-        while tmp.shape[0] < len_crop:
+        while tmp.shape[0] <= len_crop:
             idx_alt = np.random.choice(candidates)
             tmp = np.load(os.path.join(dirName, speaker, fileList[idx_alt]))
             candidates = np.delete(candidates, np.argwhere(candidates==idx_alt))
@@ -47,13 +51,27 @@ for speaker in sorted(subdirList):
         melsp = torch.from_numpy(tmp[np.newaxis, left:left+len_crop, :]).cuda()
         emb = C(melsp)
         embs.append(emb.detach().squeeze().cpu().numpy())     
-    utterances.append(np.mean(embs, axis=0))
-    
-    # create file list
-    for fileName in sorted(fileList):
-        utterances.append(os.path.join(speaker,fileName))
-    speakers.append(utterances)
-    
-with open(os.path.join(rootDir, 'train.pkl'), 'wb') as handle:
-    pickle.dump(speakers, handle)
+
+    tgt = os.path.join(targetDir, speaker)
+    if not os.path.exists(tgt):
+        os.mkdir(tgt)
+    np.save(os.path.join(tgt, speaker), np.mean(embs, axis=0).astype('float32'), allow_pickle=False)
+
+
+for i in range(0, 109, 4):
+    t1 = threading.Thread(target=spk_embedding, args=(i, ))
+    t2 = threading.Thread(target=spk_embedding, args=(i+1, ))
+    t3 = threading.Thread(target=spk_embedding, args=(i+2, ))
+    t4 = threading.Thread(target=spk_embedding, args=(i+3, ))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+
 
